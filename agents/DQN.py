01ca5317743dd58617fcff695ba3888a0c8f7e11
A. Z. Guo <albertguotrs@hotmail.com>
2019-05-19 17:47:08 -0500

    updates

--- agents/DQN.py
+++ agents/DQN.py
@@ -14,12 +14,10 @@ class Agent:
 		self.action_size = 3 # hold, buy, sell
 		self.memory = deque(maxlen=1000)
 		self.inventory = []
-
 		self.gamma = 0.95
 		self.epsilon = 1.0
 		self.epsilon_min = 0.01
 		self.epsilon_decay = 0.995
-
 		self.is_eval = is_eval
 		self.model_name = model_name
 		self.model = load_model("models/" + model_name) if is_eval else self._model()
