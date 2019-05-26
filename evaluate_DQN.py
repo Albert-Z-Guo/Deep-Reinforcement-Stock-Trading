36d21f4c0969ae0766efa9d2318b4b2ea559276b
Albert Guo <albertguotrs@hotmail.com>
2019-05-26 14:31:02 -0500

    updates

--- evaluate_DQN.py
+++ evaluate_DQN.py
@@ -1,7 +1,7 @@
 import sys
 
 import numpy as np
-np.random.seed(1) # for reproducible Keras operations
+# np.random.seed(3) # for reproducible Keras operations
 
 from keras.models import load_model
 
@@ -26,9 +26,13 @@ total_profit = 0
 window_size = state_dim
 state = generate_state(stock_prices, 0, window_size + 1)
 
+buys = []
+sells = []
+display = True
+
 for t in range(trading_period):
 	action = agent.act(state)
-
+	print(action)
 	next_state = generate_state(stock_prices, t + 1, window_size + 1)
 	reward = 0
 
@@ -36,12 +40,14 @@ for t in range(trading_period):
 	if action == 1:
 		agent.inventory.append(stock_prices[t])
 		print("Buy: " + format_price(stock_prices[t]))
+		buys.append(t)
 	# sell
 	elif action == 2 and len(agent.inventory) > 0:
 		bought_price = agent.inventory.pop(0)
 		reward = max(stock_prices[t] - bought_price, 0)
 		total_profit += stock_prices[t] - bought_price
 		print("Sell: " + format_price(stock_prices[t]) + " | Profit: " + format_price(stock_prices[t] - bought_price))
+		sells.append(t)
 	# hold
 	else:
 		pass # do nothign
@@ -54,3 +60,22 @@ for t in range(trading_period):
 		print("--------------------------------")
 		print(stock_name + " Total Profit: " + format_price(total_profit))
 		print("--------------------------------")
+
+if display:
+	import pandas as pd
+	from matplotlib import pyplot as plt
+
+	df = pd.read_csv('./data/{}.csv'.format(stock_name))
+	buy_prices = [df.iloc[t, 4] for t in buys]
+	sell_prices = [df.iloc[t, 4] for t in sells]
+
+	plt.figure(figsize=(15, 5), dpi=100)
+	plt.title('DDPG Total Profit on {}: ${:.2f}'.format(stock_name, total_profit))
+	plt.plot(df['Date'], df['Close'], color='black', label=stock_name)
+	plt.scatter(buys, buy_prices, c='green', alpha=0.5, label='buy')
+	plt.scatter(sells, sell_prices, c='red', alpha=0.5, label='sell')
+	plt.xticks(np.linspace(0, len(df), 10))
+	plt.ylabel('Price')
+	plt.legend()
+	plt.grid()
+	plt.show()
