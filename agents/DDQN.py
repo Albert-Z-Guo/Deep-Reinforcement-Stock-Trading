612c380aedfef7562bf2e0c6b788ef1b81600fc9
Albert Guo <albertguotrs@hotmail.com>
2020-02-02 22:48:15 -0500

    add Double Q-learning algorithm

---
+++ agents/DDQN.py
@@ -0,0 +1,97 @@
+import random
+from collections import deque
+
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import load_model
+from tensorflow.keras.optimizers import Adam
+
+
+# references:
+# https://arxiv.org/pdf/1802.09477.pdf
+# https://arxiv.org/pdf/1509.06461.pdf
+# https://papers.nips.cc/paper/3964-double-q-learning.pdf
+class Agent:
+    def __init__(self, state_dim, balance=50000, is_eval=False, model_name=""):
+        self.model_type = 'DQN'
+        self.state_dim = state_dim
+        self.action_dim = 3  # hold, buy, sell
+        self.memory = deque(maxlen=100)
+        self.buffer_size = 60
+        self.batch_size = 100
+        self.target_network_replacement_freq = 10
+
+
+        self.initial_portfolio_value = balance
+        self.balance = balance
+        self.inventory = []
+        self.return_rates = []
+        self.portfolio_values = [balance]
+        self.buy_dates = []
+        self.sell_dates = []
+
+        self.tau = 0.0001
+        self.gamma = 0.95
+        self.epsilon = 1.0  # initial exploration rate
+        self.epsilon_min = 0.01  # minimum exploration rate
+        self.epsilon_decay = 0.995 # decrease exploration rate as the agent becomes good at trading
+        self.is_eval = is_eval
+
+        self.model = load_model('saved_models/' + model_name) if is_eval else self.model()
+        self.model_target = load_model('saved_models/' + model_name + '_target') if is_eval else self.model
+        self.model_target.set_weights(self.model.get_weights()) # hard copy model parameters to target model parameters
+
+        self.tensorboard = TensorBoard(log_dir='./logs/DQN', update_freq=90)
+        self.tensorboard.set_model(self.model)
+
+    def update_model_target(self):
+        model_weights = self.model.get_weights()
+        model_target_weights = self.model_target.get_weights()
+        for i in range(len(model_weights)):
+            model_target_weights[i] = self.tau * model_weights[i] + (1 - self.tau) * model_target_weights[i]
+        self.model_target.set_weights(model_target_weights)
+
+    def model(self):
+        model = Sequential()
+        model.add(Dense(units=64, input_dim=self.state_dim, activation='relu'))
+        model.add(Dense(units=32, activation='relu'))
+        model.add(Dense(units=8, activation='relu'))
+        model.add(Dense(self.action_dim, activation='softmax'))
+        model.compile(loss='mse', optimizer=Adam(lr=0.001))
+        return model
+
+    def reset(self, balance):
+        self.balance = balance
+        self.inventory = []
+        self.return_rates = []
+        self.portfolio_values = [balance]
+        self.epsilon = 1.0
+
+    def remember(self, state, actions, reward, next_state, done):
+        self.memory.append((state, actions, reward, next_state, done))
+
+    def act(self, state):
+        if not self.is_eval and np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_dim)
+        options = self.model.predict(state)
+        return np.argmax(options[0])
+
+    def experience_replay(self):
+        # sample random buffer_size long memory
+        mini_batch = random.sample(self.memory, self.buffer_size)
+
+        for state, actions, reward, next_state, done in mini_batch:
+            Q_expected = reward + (1 - done) * self.gamma * np.amax(self.model_target.predict(next_state)[0])
+
+            next_actions = self.model.predict(state)
+            next_actions[0][np.argmax(actions)] = Q_expected
+            
+            history = self.model.fit(state, next_actions, epochs=1, verbose=0)
+            self.update_model_target()
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+        return history.history['loss'][0]
