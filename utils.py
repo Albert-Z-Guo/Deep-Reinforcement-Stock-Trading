e65a0d90695970392971e30d02300a1b643b5f0b
Albert Guo <albertguotrs@hotmail.com>
2020-02-03 12:43:36 -0500

    create Portfolio class abstraction

--- utils.py
+++ utils.py
@@ -4,6 +4,23 @@ from empyrical import sharpe_ratio
 from matplotlib import pyplot as plt
 
 
+class Portfolio:
+    def __init__(self, balance=50000):
+        self.initial_portfolio_value = balance
+        self.balance = balance
+        self.inventory = []
+        self.return_rates = []
+        self.portfolio_values = [balance]
+        self.buy_dates = []
+        self.sell_dates = []
+
+    def reset_portfolio(self):
+        self.balance = self.initial_portfolio_value
+        self.inventory = []
+        self.return_rates = []
+        self.portfolio_values = [self.initial_portfolio_value]
+
+        
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
